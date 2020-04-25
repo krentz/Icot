7ba5367b17b73174a61a3fcf90bcd4f12c1621c9
rafael krentz goncalves <rafael.goncalves@novus.com.br>
2020-04-25 11:35:21 +0100

    initial commit

---
+++ Icot College/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Icot College
+//
+//  Created by Rafael Krentz Gonçalves on 4/25/20.
+//  Copyright © 2020 krentz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
